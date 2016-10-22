3ac59c94cd1bf913f196421de6d34b5dee0b733f
Bruno Luis Panuto Silva <bl.panuto@gmail.com>
2016-10-22 13:50:26 -0200

    Added report

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
  * Nome Aluno: Bruno Luis Panuto Silva RA: 1510029160 *
  * Nome Aluno: Leonardo Silva RA: *
  * Data: 20/09/2016 *
-*************************************************************/
+ *************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,8 +12,7 @@
 #define OP_CADASTRAR 1
 #define OP_EXCLUIR 2
 #define OP_BUSCAR 3
-#define OP_BUSCAR_PRECO 4
-#define OP_RELATORIO 5
+#define OP_RELATORIO 4
 #define OP_SAIR 6
 #define OP_BUSCAR_NOME 1
 #define OP_BUSCAR_GENERO 2
@@ -118,7 +117,7 @@ void cleanup(char* str) {
 
 void cadastrar_produto(Produtos *P) {
   Produto p;
-  printf("==== CADASTRO DE PRODUTO ====\n");
+  printf("\n==== CADASTRO DE PRODUTO ====\n");
   printf("Digite o codigo: ");
   char code_buf[10];
   fgets(code_buf, sizeof code_buf, stdin);
@@ -134,9 +133,10 @@ void cadastrar_produto(Produtos *P) {
   cleanup(p.genero);
 
   printf("Digite o ano de fabricacao: ");
-  char ano_buf[5];
+  char ano_buf[10];
   int ano;
   fgets(ano_buf, sizeof ano_buf, stdin);
+  cleanup(ano_buf);
   ano = atoi(ano_buf);
   p.ano = ano;
 
@@ -144,6 +144,7 @@ void cadastrar_produto(Produtos *P) {
   char preco_buf[10];
   float preco;
   fgets(preco_buf, sizeof preco_buf, stdin);
+  cleanup(preco_buf);
   preco = atof(preco_buf);
   p.preco = preco;
 
@@ -153,7 +154,7 @@ void cadastrar_produto(Produtos *P) {
 
 
 void excluir_produto(Produtos* P) {
-  printf("*** Excluir produto ***\n");
+  printf("\n*** Excluir produto ***\n");
   printf("Digite o codigo do produto: ");
   char code[10];
   fgets(code, sizeof code, stdin);
@@ -162,7 +163,7 @@ void excluir_produto(Produtos* P) {
 }
 
 void buscar_produto_menu() {
-  printf("==== OPCOES DE BUSCA ====\n");
+  printf("\n==== OPCOES DE BUSCA ====\n");
   printf("1. Buscar por nome\n");
   printf("2. Buscar por genero\n");
   printf("3. Buscar por ano\n");
@@ -208,22 +209,71 @@ void buscar_produto(Produtos P){
   }
 }
 
-void buscar_produto_preco(Produtos);
-
-void relatorio(Produtos p) {
+int Produtos_buscar_preco(Produtos P, float min, float max) {
   int i = 0;
-  for(i = 0; i < p.lim; i++) {
-    Produto_print(p.ps[i]);
+  for(i = 0; i < P.lim; i++) {
+    if(P.ps[i].preco >= min && P.ps[i].preco <= max) {
+      Produto_print(P.ps[i]);
+    }
   }
 }
 
+int Produtos_qtd_produtos_genero(Produtos P) {
+  int i = 0;
+  char* generos[P.lim*2];
+  for(i = 0; i < P.lim; i++) {
+    generos[i] = P.ps[i].genero;
+  }
+  char* visited[P.lim*2];
+  int visited_lim = 0;
+  for(i = 0; i < P.lim; i++) {
+    char* genero = generos[i];
+
+    int qtd = 1, j = 0;
+    for(j = i+1; j < P.lim; j++) {
+      if((strcmp(generos[j], genero)) == 0) {
+        qtd++;
+      }
+    }
+    int k = 0, was_printed = 0;
+    for(k = 0; k < visited_lim; k++) {
+      if((strcmp(visited[k], genero)) == 0) {
+        was_printed = 1;
+      }
+    }
+    if(!was_printed) {
+      printf("Genero %s tem %d produtos\n", genero, qtd);
+      visited[visited_lim++] = genero;
+    }
+  }
+}
+
+void relatorio(Produtos P) {
+  printf("\n*** Relatorio ***\n");
+  float p_min = 0;
+  char p_min_buf[10];
+  printf("Digite um preço minimo: ");
+  fgets(p_min_buf, sizeof p_min_buf, stdin);
+  p_min = atof(p_min_buf);
+
+  float p_max = 0;
+  char p_max_buf[10];
+  printf("Digite um preço maximo: ");
+  fgets(p_max_buf, sizeof p_max_buf, stdin);
+  p_max = atof(p_max_buf);
+
+  printf("\n*** Produtos nessa faixa de preco ***\n");
+  handle_err(Produtos_buscar_preco(P, p_min, p_max));
+  printf("\n*** Quantidade de produtos por gênero ***\n");
+  handle_err(Produtos_qtd_produtos_genero(P));
+}
+
 void menu() {
-  printf("==== SUPERMERCADO STO ANTONIO ====\nMenu Principal\n");
+  printf("\n==== SUPERMERCADO STO ANTONIO ====\nMenu Principal\n");
   printf("1. Cadastrar produto\n");
   printf("2. Excluir produto\n");
   printf("3. Buscar produto\n");
-  printf("4. Buscar produto por preco\n");
-  printf("5. Relatorio\n");
+  printf("4. Relatorio\n");
   printf("6. Sair\n");
 }
 
@@ -247,9 +297,6 @@ int main() {
       break;
     case OP_BUSCAR:
       buscar_produto(p);
-      break;
-    case OP_BUSCAR_PRECO:
-      // TODO buscar_produto_preco(p);
       break;
     case OP_RELATORIO:
       relatorio(p);
